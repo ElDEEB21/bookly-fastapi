37f5e5efc7c16637d8fb3b74bc172efac8fd6612
ElDEEB21 <omer84484@gmail.com>
2026-08-27 02:53:14 +0300

    feat: add auth models and schemas - User model with role field, UserCreateModel, UserModel, UserLoginModel

---
+++ src/auth/schemas.py
@@ -0,0 +1,25 @@
+from pydantic import BaseModel, Field
+import uuid
+from datetime import datetime
+
+class UserCreateModel(BaseModel):
+    first_name: str = Field(min_length=1, max_length=100)
+    last_name: str = Field(min_length=1, max_length=100)
+    username: str = Field(max_length=8)
+    email: str = Field(max_length=128)
+    password: str = Field(min_length=8)
+
+class UserModel(BaseModel):
+    uid: uuid.UUID
+    username: str
+    email: str
+    first_name: str
+    last_name: str
+    is_verified: bool
+    password_hash: str = Field(exclude=True)
+    created_at: datetime
+    updated_at: datetime
+
+class UserLoginModel(BaseModel):
+    email: str = Field(max_length=128)
+    password: str = Field(min_length=8)
