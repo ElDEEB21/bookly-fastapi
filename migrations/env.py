5640e94adb46c67240487469a1935295e52f1fbb
ElDEEB21 <omer84484@gmail.com>
2026-08-29 20:28:15 +0300

    fix: add missing booktag and tags tables with migration

--- migrations/env.py
+++ migrations/env.py
@@ -7,7 +7,7 @@ from sqlalchemy.ext.asyncio import async_engine_from_config
 
 from alembic import context
 from src.config import Config
-from src.db.models import User, Book
+from src.db.models import User, Book, Tag, BookTag
 from sqlmodel import SQLModel
 
 database_url = Config.DATABASE_URL
