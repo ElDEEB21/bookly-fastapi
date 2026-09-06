a3769e2ef3d4f5303ed9645c93e04be80f37a16f
ElDEEB21 <omer84484@gmail.com>
2026-09-06 20:13:38 +0300

    Remove unused Authorization middleware

--- src/middleware.py
+++ src/middleware.py
@@ -1,11 +1,10 @@
 import logging
 import time
 
-from fastapi import FastAPI, status
+from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.middleware.trustedhost import TrustedHostMiddleware
 from fastapi.requests import Request
-from fastapi.responses import JSONResponse
 
 logger = logging.getLogger("uvicorn.access")
 logger.disabled = True
@@ -23,20 +22,6 @@ def register_middleware(app: FastAPI):
                    f"{response.status_code} completed after {processing_time}s")
 
         print(message)
-        return response
-
-    @app.middleware("http")
-    async def authorization(request: Request, call_next):
-        if not "Authorization" in request.headers:
-            return JSONResponse(
-                content={
-                    "message": "Not Authenticated",
-                    "resolution": "Please provide the right credentials"
-                },
-                status_code=status.HTTP_401_UNAUTHORIZED
-            )
-
-        response = await call_next(request)
         return response
 
     app.add_middleware(
